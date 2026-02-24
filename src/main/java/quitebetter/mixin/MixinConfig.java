2cb81d41227c374acfaa1dcb65073476a5c50d82
Meganobody <78742906+Meganobody@users.noreply.github.com>
2026-02-24 09:24:49 +0500

    Revert "Merge pull request #7 from SmushyTaco/main"
    This reverts commit d14be0ab8d0b01c1c57d00f93b9ffc9a4db2a239, reversing
    changes made to 29bad05662efa6f908688bf8888e5c8e83aec4f8.

--- src/main/java/quitebetter/mixin/MixinConfig.java
+++ src/main/java/quitebetter/mixin/MixinConfig.java
@@ -5,13 +5,14 @@ import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 public class MixinConfig implements IMixinConfigPlugin {
 	@Override
-	public void onLoad(String mixinPackage) {}
+	public void onLoad(String mixinPackage) {
+
+	}
 
 	@Override
 	public String getRefMapperConfig() {
@@ -20,23 +21,31 @@ public class MixinConfig implements IMixinConfigPlugin {
 
 	@Override
 	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-		if (targetClassName.equals("net.minecraft.core.entity.EntityFishingBobber")) {
-			return !FabricLoaderImpl.INSTANCE.isModLoaded("stardew");
+		switch (targetClassName) {
+			case "net.minecraft.core.entity.EntityFishingBobber":
+				return !FabricLoaderImpl.INSTANCE.isModLoaded("stardew");
+			default:
+				return true;
 		}
-		return true;
 	}
 
 	@Override
-	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+	}
 
 	@Override
 	public List<String> getMixins() {
-		return Collections.emptyList();
+		return null;
 	}
 
 	@Override
-	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
 
 	@Override
-	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
 }
