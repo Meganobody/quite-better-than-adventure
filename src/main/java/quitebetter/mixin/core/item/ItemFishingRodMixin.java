f9e32f0f6aa9e097451e14eb2b1d8eb4ba4ce5dc
Meganobody <78742906+Meganobody@users.noreply.github.com>
2026-02-25 17:57:49 +0500

    Reapply "Merge pull request #7 from SmushyTaco/main"
    This reverts commit 2cb81d41227c374acfaa1dcb65073476a5c50d82.

--- src/main/java/quitebetter/mixin/core/item/ItemFishingRodMixin.java
+++ src/main/java/quitebetter/mixin/core/item/ItemFishingRodMixin.java
@@ -1,25 +1,19 @@
 package quitebetter.mixin.core.item;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemFishingRod;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(
-	value = {ItemFishingRod.class},
-	remap = false
-)
+import java.util.Objects;
+
+@Mixin(value = {ItemFishingRod.class}, remap = false)
 public class ItemFishingRodMixin {
-
-	@Inject(
-		method = {"<init>"},
-		at = @At("TAIL")
-	)
-	public void ItemFishingRod(String name, String namespaceId, int id, CallbackInfo ci) {
-		if (name == "tool_steel_fishingrod") {
-			ItemFishingRod rod = (ItemFishingRod)(Object)this;
-			rod.setMaxDamage(588);
-		}
+	@WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/item/ItemFishingRod;setMaxDamage(I)Lnet/minecraft/core/item/Item;"))
+	private Item itemFishingRod(ItemFishingRod instance, int i, Operation<Item> original, String name, String namespaceId, int id) {
+		if (Objects.equals(name, "tool_steel_fishingrod")) return original.call(instance, 588);
+		return original.call(instance, i);
 	}
 }
